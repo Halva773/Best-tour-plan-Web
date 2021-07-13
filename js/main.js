7e2181d434e5d33b84d0ed05ccda906a91951c02
Artem-HN <48379705+Halva773@users.noreply.github.com>
2021-07-13 13:21:46 +0300

    Added subscribe and hotel container and 2nd slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSwiper = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel__slider-button--next',
+    prevEl: '.hotel__slider-button--prev',
+  },
+});
+
+const feedbackSwiper = new Swiper('.feedback-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.feedback__swiper-button--next',
+    prevEl: '.feedback__swiper-button--prev',
   },
 });
